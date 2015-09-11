1600e68d01a4782ee9e823477b15980e7e2051ae
James Jacoby <james@builtbymoby.com>
2015-09-10 22:37:12 -0700

    improved motion

--- bubbles_flat/jquery-waft.js
+++ bubbles_flat/jquery-waft.js
@@ -1,5 +1,5 @@
 ;
-(function($, window, document, undefined) {
+(function($, window, document, _, undefined) {
   'use strict';
 
   var pluginName = 'waft',
@@ -28,7 +28,8 @@
     this.scrollSMA = simpleMovingAverager(this.scrollPeriod);
     this.smoothScrollVelocity = 0;
     this.friction = 0.8;
-    this.elasticForce = 2.0;
+    this.elasticForce = 8.0;
+    this.elasticDampen = 0.9;
     this.animFrame =  window.requestAnimationFrame       ||
                       window.webkitRequestAnimationFrame ||
                       window.mozRequestAnimationFrame    ||
@@ -55,6 +56,15 @@
     }
   };
 
+  function randomRange( $min, $max, $precision )
+  {
+    if( typeof( $precision ) == 'undefined')
+    {
+      $precision = 2;
+    }
+    return parseFloat( Math.min( $min + ( Math.random() * ( $max - $min ) ), $max ).toFixed( $precision ) );
+  };
+
   $.extend(Plugin.prototype, {
     init: function() {
       this.initModel();
@@ -71,8 +81,21 @@
           initialPos: parseFloat(tmp.css('top')),
           pos: 0,
           delta: 0,
-          velocity: 0
+          velocity: 0,
+          mod: 1
         };
+
+        if (tmp.hasClass('case-study')) {
+          node.mod = randomRange(0.8, 1.0);
+        } else if (tmp.hasClass('product') || tmp.hasClass('cta' )) {
+          node.mod = randomRange(0.6, 0.7);
+        } else if (tmp.hasClass('blog')) {
+          node.mod = randomRange(0.4, 0.5);
+        } else if (tmp.hasClass('job')) {
+          node.mod = randomRange(0.3, 0.4);
+        } else {
+          node.mod = 0.2;
+        }
 
         self.nodes.push(node);
       });
@@ -81,12 +104,12 @@
       var self = this;
       self.lastScrollTop = self.$window.scrollTop();
 
-      self.$window.scroll(function(e) {
+      self.$window.scroll(_.throttle(function(e) {
         var newTop = self.$window.scrollTop();
         var velocity = newTop - self.lastScrollTop;
         self.lastScrollTop = newTop;
         self.smoothScrollVelocity = self.scrollSMA(velocity);
-      });
+      }, 100));
     },
     startMainLoop: function() {
       var self = this;
@@ -114,36 +137,42 @@
     tick: function(delta) {
       var s = delta / 1000;
 
-      if (this.$fpsElement !== null) this.updateFPS(delta);
+      //if (this.$fpsElement !== null) this.updateFPS(delta);
 
       // scroll velocity friction
       this.smoothScrollVelocity *= this.friction * (1 - s);
-      var force = this.smoothScrollVelocity * 0.01;
+      var force = -this.smoothScrollVelocity * 0.02;
 
       for (var i=0; i<this.nodes.length; i++) {
         var node = this.nodes[i];
         // apply force
-        node.velocity += force;
+        node.velocity += force * node.mod;
 
         var dist = Math.abs(node.delta);
         var mod = dist * 0.1;
+
+        node.delta += node.velocity;
+
         // apply elastic resistance
         if (node.delta > 0) {
           node.velocity -= this.elasticForce * s * mod;
           if (node.delta + node.velocity < 0) {
             node.delta = 0;
-            node.velocity = 0;
+            // node.velocity = 0;
           }
         } else if (node.delta < 0) {
           node.velocity += this.elasticForce * s * mod;
           if (node.delta + node.velocity > 0) {
             node.delta = 0;
-            node.velocity = 0;
+            // node.velocity = 0;
           }
         }
 
-        node.delta += node.velocity;
+        node.velocity *= this.elasticDampen;
 
+        if (Math.abs(node.velocity) < 0.05) {
+          node.velocity = 0;
+        }
         // update position
         node.pos = node.initialPos + node.delta;
       }
@@ -176,4 +205,4 @@
     });
   };
 
-})(jQuery, window, document);
+})(jQuery, window, document, _);
